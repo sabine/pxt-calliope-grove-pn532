1b795c4e53c1859c95446fb199f3964555ce9186
sabine <sabineschmaltz@gmail.com>
2019-10-02 08:15:02 +0200

    ,

--- grove-pn532.ts
+++ grove-pn532.ts
@@ -105,7 +105,7 @@ namespace grove_pn532 {
 
         }
 
-        if (DEBUG_SERIAL) debug_message("Got outputBuffer! "+JSON.stringify(outputFrame));
+        if (DEBUG_SERIAL) debug_message("Got outputBuffer! "+decToHex(outputFrame));
 
         return outputFrame;
     }
